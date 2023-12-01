1ac322befa04ad73b3c566e0cdbc6995e9e9820d
Thamim Ansari <thamimansari1626@gmail.com>
2023-12-01 19:22:46 +0000

    Task Completed

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,61 @@
-// Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {eachItem, startButton} = props
+  const {title, date, star, id} = eachItem
+  const formatedDate = new Date(date)
+  const dayArray = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wenessday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  const startClick = () => {
+    startButton(id)
+  }
+  const year = formatedDate.getFullYear()
+  const month = monthNames[formatedDate.getMonth()]
+  const dates = formatedDate.getDate()
+  const day = dayArray[formatedDate.getDay()]
+
+  const imgUrl = star
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  return (
+    <li className="item-card">
+      <div className="top-item">
+        <p className="title">{title}</p>
+        <button data-testid="star" className="star-btn">
+          <img
+            src={imgUrl}
+            alt="star"
+            className="star-img"
+            onClick={startClick}
+          />
+        </button>
+      </div>
+      <p className="bottom-item">{`Date : ${dates} ${month} ${year}, ${day}`}</p>
+    </li>
+  )
+}
+
+export default AppointmentItem
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,18 +1,86 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import AppointmentItem from '../AppointmentItem'
 import './index.css'
 
+let initialList = localStorage.getItem('initialList')
+
+if (initialList === null) {
+  initialList = []
+} else {
+  initialList = JSON.parse(initialList)
+}
+
 class Appointments extends Component {
-  state = {titleInpu: '', dateInput: '', appoinmentList: []}
+  state = {
+    titleInput: '',
+    dateInput: null,
+    staredButton: false,
+    appoinmentList: initialList,
+  }
+
+  onClickStarButton = () => {
+    this.setState(prevstate => ({staredButton: !prevstate.staredButton}))
+  }
+
+  startButton = ids => {
+    const {appoinmentList} = this.state
+    const startedAppoinmentList = appoinmentList.map(eachItem => {
+      const {id, title, date, star} = eachItem
+      const changeStar = {
+        id,
+        title,
+        date,
+        star: !star,
+      }
+      if (eachItem.id === ids) {
+        return changeStar
+      }
+      return eachItem
+    })
+    this.setState({appoinmentList: startedAppoinmentList})
+    localStorage.setItem('initialList', JSON.stringify(startedAppoinmentList))
+  }
+
+  onChangeText = event => {
+    this.setState({titleInput: event.target.value})
+  }
+
+  onChangeDate = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  onSubmit = event => {
+    event.preventDefault()
+    const {titleInput, dateInput, appoinmentList} = this.state
+    const singleAppointment = {
+      id: uuidv4(),
+      title: titleInput,
+      date: dateInput,
+      star: false,
+    }
+    this.setState({appoinmentList: [...appoinmentList, singleAppointment]})
+    this.setState({titleInput: ''})
+    this.setState({dateInput: null})
+  }
 
   render() {
-    const {appoinmentList} = this.state
+    const {appoinmentList, titleInput, dateInput, staredButton} = this.state
+    let filterStarArray
+    if (staredButton) {
+      filterStarArray = appoinmentList.filter(
+        eachItem => eachItem.star === true,
+      )
+    } else {
+      filterStarArray = appoinmentList
+    }
     return (
       <div className="container">
         <div className="card">
           <div className="top-section">
             <div className="top-left-section">
-              <h1 className="heading">Apointment</h1>
-              <from className="form-element">
+              <h1 className="heading">Add Appointment</h1>
+              <form className="form-element" onSubmit={this.onSubmit}>
                 <label className="label" htmlFor="textInput">
                   Title
                 </label>
@@ -22,6 +90,8 @@ class Appointments extends Component {
                   className="input"
                   placeholder="title"
                   id="textInput"
+                  onChange={this.onChangeText}
+                  value={titleInput}
                 />
                 <br />
                 <label className="label" htmlFor="dateInput">
@@ -33,110 +103,42 @@ class Appointments extends Component {
                   className="input"
                   placeholder="dd/mm/yyyy"
                   id="dateInput"
+                  onChange={this.onChangeDate}
+                  value={dateInput}
                 />
                 <br />
-                <button type="submit" data-testid="add" className="add-button">
+                <button type="submit" className="add-button">
                   Add
                 </button>
-              </from>
+              </form>
             </div>
 
             <img
               src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png "
-              alt="apoinment"
+              alt="appointments"
               className="top-right-section"
             />
           </div>
           <hr />
           <div className="bottom-section">
             <div className="bottom-top">
-              <h2 className="sub-heading">Appoinments</h2>
-              <button className="star-button"> stared</button>
+              <h2 className="sub-heading">Appointments</h2>
+              <button
+                className="star-button"
+                type="button"
+                onClick={this.onClickStarButton}
+              >
+                Starred
+              </button>
             </div>
             <ul className="bottom-bottom">
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
-
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
-
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
-
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
-
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
-
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
-
-              <li className="item-card">
-                <div className="top-item">
-                  <p className="title">tille</p>
-                  <img
-                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
-                    alt="favorite"
-                    className="star-img"
-                  />
-                </div>
-                <p className="bottom-item">date monday 12/23/2023</p>
-              </li>
+              {filterStarArray.map(eachItem => (
+                <AppointmentItem
+                  eachItem={eachItem}
+                  startButton={this.startButton}
+                  key={eachItem.id}
+                />
+              ))}
             </ul>
           </div>
         </div>
